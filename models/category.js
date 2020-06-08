be358b208e450c0c5dedd682ad37fbd4dea6014c
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-08 11:36:25 +0200

    created category model

---
+++ models/category.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define(
+    "category",
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+    },
+    {}
+  );
+  category.associate = function (models) {
+    // associations can be defined here
+  };
+  return category;
+};
